1ce36d8a7bc13c5cc60b7f927ca0b78e44c0ec03
Alex <covertpluto3502@gmail.com>
2022-04-26 18:40:41 +0100

    small changes

--- brightness_changer.py
+++ brightness_changer.py
@@ -1,5 +1,5 @@
 import pystray
-from PIL import Image
+from PIL import Image, ImageOps
 import time
 import win32api
 from monitorcontrol import get_monitors
@@ -7,27 +7,20 @@ from winreg import HKEY_CURRENT_USER as hkey, QueryValueEx as getSubkeyValue, Op
 
 
 def theme():
-    """ Uses the Windows Registry to detect if the user is using Dark Mode """
-    # Registry will return 0 if Windows is in Dark Mode and 1 if Windows is in Light Mode. This dictionary converts that output into the text that the program is expecting.
-    valueMeaning = {0: "Dark", 1: "Light"}
-    # In HKEY_CURRENT_USER, get the Personalisation Key.
+    value_meaning = {0: "Dark", 1: "Light"}
     try:
         key = getKey(hkey, "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize")
-        # In the Personalisation Key, get the AppsUseLightTheme subkey. This returns a tuple.
-        # The first item in the tuple is the result we want (0 or 1 indicating Dark Mode or Light Mode); the other value is the type of subkey e.g. DWORD, QWORD, String, etc.
         subkey = getSubkeyValue(key, "AppsUseLightTheme")[0]
     except FileNotFoundError:
         # some headless Windows instances (e.g. GitHub Actions or Docker images) do not have this key
         return None
-    return valueMeaning[subkey]
+    return value_meaning[subkey]
 
-def isDark():
-    if theme() is not None:
-        return theme() == 'Dark'
 
 def isLight():
     if theme() is not None:
         return theme() == 'Light'
+
 
 def change_luminance():
     global index
@@ -39,9 +32,6 @@ def change_luminance():
         index += 1
     else:
         index = 0
-
-
-
 
 
 def change():
@@ -68,7 +58,14 @@ def stop():
 
 def load_image(path):
     try:
-        return Image.open(path)
+        img = Image.open(path)
+        if isLight():
+            # invert the image if the user is using light mode
+            img = ImageOps.invert(img)
+            return img
+        else:
+            # if the user is using dark mode, return the image as is
+            return img
     except FileNotFoundError:
         win32api.MessageBox(0, "Assets not found. Press OK to download...", "Icons not found")
         import requests
@@ -81,7 +78,9 @@ def load_image(path):
             r = requests.get(img)
             with open(img.split("/")[-1], "wb") as f:
                 f.write(r.content)
-        return Image.open(path)
+        # try doing it again having found the icons
+        return load_image(path)
+
 
 # change this to False if you are sure that your monitor is definitely compatible with this script
 quit_on_error = True
@@ -91,7 +90,7 @@ quit_on_error = True
 #  laptop.ico
 #  monitor.ico
 #  brightness.ico
-image = load_image('laptop.ico')
+image = load_image('brightness.ico')
 
 icon = pystray.Icon(
     'icon',
@@ -102,13 +101,8 @@ icon = pystray.Icon(
      )
 )
 
-
 brightnesses = [50, 75, 100, 1, 25]
 index = 1
-
-
-
-
 
 
 def set_all(luminance):
